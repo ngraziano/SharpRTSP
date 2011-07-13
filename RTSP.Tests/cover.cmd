ffd2b14c48d6bbc8855ab2c5c96cb8abd08be8e9
Nicolas GRAZIANO <nicolas.graziano@gmail.com>
2011-07-13 22:03:26 +0200

    Add test execution and coverage.

---
+++ RTSP.Tests/cover.cmd
@@ -0,0 +1,8 @@
+
+IF NOT DEFINED OPENCOVER SET OPENCOVER="%ProgramFiles%\OpenCover"
+IF NOT DEFINED NUNIT SET NUNIT="%ProgramFiles%\NUnit 2.5.9\bin\net-2.0"
+
+
+%OPENCOVER%\OpenCover.Console.exe -register:user -target:%NUNIT%\nunit-console.exe -targetargs:"/noshadow RTSPTests.nunit"  -filter:+[*]RTSP* -output:cover.xml
+
+D:\temp\Report\reportGenerator "cover.xml" Coverage
