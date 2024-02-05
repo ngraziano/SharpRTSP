939c12cb14de0fb7c2f9e29e8d81665089e1317f
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-02-05 19:59:52 +0100

    Small program to test package with dotnetframework

---
+++ TestConsoleDotNetFramework/Program.cs
@@ -0,0 +1,39 @@
+﻿using Rtsp;
+using Rtsp.Messages;
+using Rtsp.Rtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RtspUtils.RegisterUri();
+            RtspTcpTransport transport = new RtspTcpTransport(new Uri(args[0]));
+            RtspListener listener = new RtspListener(transport);
+
+            listener.MessageReceived += (sender, e) =>
+            {
+                Console.WriteLine("Received " + e.Message);
+            };
+            listener.Start();
+
+            RtspRequest optionsMessage = new RtspRequestOptions();
+            listener.SendMessage(optionsMessage);
+
+            RtspRequest describeMessage = new RtspRequestDescribe();
+            listener.SendMessage(describeMessage);
+
+            
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
