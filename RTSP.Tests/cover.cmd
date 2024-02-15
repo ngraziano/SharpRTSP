fff3ad46ec1cbc276ce8fc8f067d5daf2b3ed250
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-02-15 09:50:10 +0100

    Remove obsolete files

--- RTSP.Tests/cover.cmd
+++
@@ -1,8 +0,0 @@
-
-IF NOT DEFINED OPENCOVER SET OPENCOVER="%ProgramFiles%\OpenCover"
-IF NOT DEFINED NUNIT SET NUNIT="%ProgramFiles%\NUnit 2.5.9\bin\net-2.0"
-
-
-%OPENCOVER%\OpenCover.Console.exe -register:user -target:%NUNIT%\nunit-console.exe -targetargs:"/noshadow RTSPTests.nunit"  -filter:+[*]Rtsp* -output:cover.xml
-
-D:\temp\Report\reportGenerator "cover.xml" Coverage
