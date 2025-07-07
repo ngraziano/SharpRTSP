a0986f46dd4a2089370967773dc91548d04dbd84
Nicolas Graziano <nicolas.graziano@gmail.com>
2025-07-07 17:21:59 +0200

    Simplify rawPayload
    Remove unused assignment, use modern syntax for slice, remove unused using.

--- RTSP/Rtp/RawPayload.cs
+++ RTSP/Rtp/RawPayload.cs
@@ -1,8 +1,5 @@
 ﻿using Rtsp.Onvif;
-using Rtsp.Utils;
-using System;
 using System.Buffers;
-using System.Collections.Generic;
 
 namespace Rtsp.Rtp
 {
@@ -12,14 +9,14 @@ namespace Rtsp.Rtp
 
         public RawPayload(MemoryPool<byte>? memoryPool = null)
         {
-            _memoryPool = memoryPool ??= MemoryPool<byte>.Shared;
+            _memoryPool = memoryPool ?? MemoryPool<byte>.Shared;
         }
 
         public RawMediaFrame ProcessPacket(RtpPacket packet)
         {
             var memoryOwner = _memoryPool.Rent(packet.PayloadSize);
             packet.Payload.CopyTo(memoryOwner.Memory.Span);
-            return new RawMediaFrame(new ReadOnlySequence<byte>(memoryOwner.Memory.Slice(0, (packet.PayloadSize))), memoryOwner)
+            return new RawMediaFrame(new ReadOnlySequence<byte>(memoryOwner.Memory[..packet.PayloadSize]), memoryOwner)
             {
                 ClockTimestamp = RtpPacketOnvifUtils.ProcessRTPTimestampExtension(packet.Extension, headerPosition: out _),
                 RtpTimestamp = packet.Timestamp,
