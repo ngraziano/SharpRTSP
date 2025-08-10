649be8f126631d05a7fa7bd96d629e29a9af80ea
Nicolas Graziano <nicolas.graziano@gmail.com>
2025-08-10 12:21:00 +0200

    Tunnel Http server
    Initial version of rtsp server over http tunnel.

---
+++ RTSP/RtspHttpServerTransport.cs
@@ -0,0 +1,234 @@
+﻿namespace Rtsp;
+
+using System;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class RtspHttpServerTransport : IRtspTransport, IDisposable
+{
+    private class HttpTransportStream : Stream
+    {
+        private readonly Stream _outStream;
+        private readonly RtspHttpServerTransport _parent;
+
+        public HttpTransportStream(RtspHttpServerTransport parent)
+        {
+            Debug.Assert(parent._getChannelClient != null);
+            _outStream = parent._getChannelClient!.GetStream();
+            _parent = parent;
+        }
+
+        public override bool CanRead => _outStream.CanRead;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => _outStream.CanWrite;
+
+        public override long Length => throw new NotSupportedException("Not supported in network");
+
+        public override long Position
+        {
+            get => throw new NotSupportedException("Not supported in network");
+            set => throw new NotSupportedException("Not supported in network");
+        }
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException("Not supported in network");
+
+        public override void SetLength(long value) => throw new NotSupportedException("Not supported in network");
+
+        public override void Flush() => _outStream.Flush();
+
+#if NET8_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            var reader = _parent._decodedDataPipe.Reader;
+            var result = await reader.ReadAtLeastAsync(buffer.Length, cancellationToken).ConfigureAwait(false);
+
+            result.Buffer.Slice(0, buffer.Length).CopyTo(buffer.Span);
+            var handlePart = result.Buffer.GetPosition(buffer.Length);
+            reader.AdvanceTo(handlePart, handlePart);
+
+            return buffer.Length;
+        }
+
+        // Remove a copy when possible
+        public override void Write(ReadOnlySpan<byte> buffer) => _outStream.Write(buffer);
+#endif
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            var reader = _parent._decodedDataPipe.Reader;
+            var result = await reader.ReadAtLeastAsync(count, cancellationToken).ConfigureAwait(false);
+
+            result.Buffer.Slice(0, count).CopyTo(buffer.AsSpan(offset, count));
+            var handlePart = result.Buffer.GetPosition(count);
+            reader.AdvanceTo(handlePart, handlePart);
+
+            return count;
+        }
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return ReadAsync(buffer, offset, count, default).Result;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count) => _outStream.Write(buffer, offset, count);
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _outStream.Dispose();
+                _parent.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+    private TcpClient? _postChannelClient;
+    private TcpClient? _getChannelClient;
+    private Stream? _stream;
+    private uint _commandCounter;
+    private bool _disposedValue;
+    private readonly Pipe _decodedDataPipe = new();
+    private readonly CancellationTokenSource _stop = new();
+
+
+    internal enum UpdateState
+    {
+        Ok,
+        NewSession,
+        Error,
+    }
+
+    public string RemoteAddress => RemoteEndPoint.ToString();
+
+    public IPEndPoint RemoteEndPoint => _getChannelClient?.Client.RemoteEndPoint as IPEndPoint ?? throw new InvalidOperationException("The local endpoint can not be determined.");
+
+    public IPEndPoint LocalEndPoint => _getChannelClient?.Client.LocalEndPoint as IPEndPoint ?? throw new InvalidOperationException("The local endpoint can not be determined.");
+
+    public bool Connected => _getChannelClient?.Connected == true;
+
+    internal RtspHttpServerTransport() { }
+
+    public void Close()
+    {
+        _postChannelClient?.Close();
+        _getChannelClient?.Close();
+    }
+
+    public Stream GetStream() => _stream ?? throw new InvalidOperationException("Invalid internal state");
+
+    public uint NextCommandIndex() => ++_commandCounter;
+
+    public void Reconnect() => throw new InvalidOperationException("Server can not reconnect to client");
+
+    internal UpdateState UpdatePostChannel(TcpClient client)
+    {
+        var wasPresent = _postChannelClient != null;
+        _postChannelClient?.Close();
+
+        _postChannelClient = client;
+        _ = Task.Factory.StartNew(async () => await DecodePostChannel(_stop.Token).ConfigureAwait(false),
+            _stop.Token,
+            TaskCreationOptions.LongRunning,
+            TaskScheduler.Current);
+
+        if (!wasPresent && _getChannelClient != null)
+        {
+            return UpdateState.NewSession;
+        }
+        return UpdateState.Ok;
+    }
+
+    private async Task DecodePostChannel(CancellationToken token)
+    {
+        Debug.Assert(_postChannelClient != null);
+        try
+        {
+            var pipeSource = PipeReader.Create(_postChannelClient!.GetStream());
+            var pipeDest = _decodedDataPipe.Writer;
+            while (!token.IsCancellationRequested)
+            {
+
+                var sourceReadResult = await pipeSource.ReadAsync(token).ConfigureAwait(false);
+                var sourceBuffer = sourceReadResult.Buffer;
+
+                var roundLength = (int)(sourceBuffer.Length / 4 * 4);
+                // ask a buffer too big but it allow to use it as temporary buffer
+                var decodedBuffer = pipeDest.GetSpan((int)sourceBuffer.Length);
+                sourceBuffer.CopyTo(decodedBuffer);
+                var decodeResult = Base64.DecodeFromUtf8InPlace(decodedBuffer[..roundLength], out int written);
+                if (decodeResult == OperationStatus.Done)
+                {
+                    pipeDest.Advance(written);
+                    var sourcePosition = sourceBuffer.GetPosition(roundLength);
+                    pipeSource.AdvanceTo(sourcePosition, sourcePosition);
+                    FlushResult flushResult = await pipeDest.FlushAsync(token).ConfigureAwait(false);
+                    if (flushResult.IsCompleted)
+                    {
+                        // reader is closed
+                        break;
+                    }
+                    if (sourceReadResult.IsCompleted)
+                    {
+                        // source tcp is closed
+                        break;
+                    }
+
+                }
+                else if (decodeResult != OperationStatus.NeedMoreData)
+                {
+                    throw new InvalidOperationException("Fail to decode base64 post channel");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // IGNORE
+        }
+        catch (IOException)
+        {
+            // IGNORE
+        }
+        _postChannelClient?.Dispose();
+
+    }
+
+    internal UpdateState UpdateGetChannel(TcpClient client)
+    {
+        if (_getChannelClient != null) return UpdateState.Error;
+
+        _getChannelClient = client;
+        _stream = new HttpTransportStream(this);
+
+        if (_postChannelClient != null)
+        {
+            return UpdateState.NewSession;
+        }
+        return UpdateState.Ok;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                Close();
+            }
+            _disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ RTSP/RtspOverHttpListenSocket.cs
@@ -0,0 +1,228 @@
+﻿namespace Rtsp;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Rtsp.Messages;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class RtspOverHttpListenSocket
+{
+    private readonly TcpListener _tcpListener;
+    private readonly ILogger _logger;
+    private CancellationTokenSource _stop = new();
+
+    private readonly BlockingCollection<RtspHttpServerTransport> _newConnections = new(100);
+    private readonly ConcurrentDictionary<string, RtspHttpServerTransport> _activesSessions = new(StringComparer.Ordinal);
+
+    private readonly byte[] getResponse = Encoding.ASCII.GetBytes("HTTP/1.0 200 OK\r\n"
+                    + "Server: SharpRTSP\r\n"
+                    + "Connection: close\r\n"
+                    + "Date: Thu, 19 Aug 1982 18:30:00 GMT\r\n"
+                    + "Cache-Control: no-store\r\n"
+                    + "Pragma: no-cache\r\n"
+                    + "Content-Type: application/x-rtsp-tunnelled\r\n"
+                    + "\r\n");
+
+    public RtspOverHttpListenSocket(TcpListener tcpListener, ILogger<RtspOverHttpListenSocket>? logger = null)
+    {
+        _tcpListener = tcpListener;
+        _logger = logger as ILogger ?? NullLogger.Instance;
+    }
+
+    public IRtspTransport Accept()
+    {
+        return _newConnections.Take(_stop.Token);
+    }
+
+    public void Start()
+    {
+        // stop old one
+        _stop.Cancel();
+
+        _stop = new();
+        _tcpListener.Start();
+        _ = Task.Factory.StartNew(async () => await AcceptConnections(_stop.Token).ConfigureAwait(false),
+            _stop.Token,
+            TaskCreationOptions.LongRunning,
+            TaskScheduler.Current);
+    }
+
+    public void Stop()
+    {
+        _stop.Cancel();
+        _tcpListener.Stop();
+    }
+
+    private async Task AcceptConnections(CancellationToken cancellationToken)
+    {
+        try
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+#if NET8_0_OR_GREATER
+                var client = await _tcpListener.AcceptTcpClientAsync(cancellationToken).ConfigureAwait(false);
+#else
+                var client = await _tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
+#endif
+                _logger.LogDebug("Connection from {remoteEndPoint}", client.Client.RemoteEndPoint);
+                await HandleHeaderAndAddToSessions(client, cancellationToken).ConfigureAwait(false);
+
+                // TODO cleanup old session
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            _logger.LogDebug("Accept connections canceled");
+        }
+    }
+
+    private async Task HandleHeaderAndAddToSessions(TcpClient client, CancellationToken cancellationToken)
+    {
+        // prevent bad client to totally block the system
+        client.ReceiveTimeout = 5000;
+        try
+        {
+            var clientStream = client.GetStream();
+            var firstLine = await ReadOneLine(clientStream, cancellationToken).ConfigureAwait(false);
+
+            bool isPostChannel;
+            var parts = firstLine?.Split(' ') ?? [];
+            switch (parts.Length)
+            {
+                case 3 when string.Equals(parts[0], "GET", StringComparison.OrdinalIgnoreCase):
+                    isPostChannel = false;
+                    break;
+                case 3 when string.Equals(parts[0], "POST", StringComparison.OrdinalIgnoreCase):
+                    isPostChannel = true;
+                    break;
+                default:
+                    _logger.LogWarning("Invalid message receive {message}", firstLine);
+                    client.Dispose();
+                    return;
+            }
+            Dictionary<string, string> headers = await ReadHeaders(clientStream, cancellationToken).ConfigureAwait(false);
+            client.ReceiveTimeout = 0;
+
+            if (!headers.TryGetValue("x-sessioncookie", out var sessionCookie))
+            {
+                _logger.LogWarning("No session cookie find");
+                client.Dispose();
+                return;
+            }
+
+            if (!_activesSessions.TryGetValue(sessionCookie, out var session))
+            {
+                _logger.LogDebug("Create session {sessionCookie}", sessionCookie);
+                session = new();
+                _activesSessions[sessionCookie] = session;
+            }
+
+            if (isPostChannel)
+            {
+                if (!headers.TryGetValue(RtspHeaderNames.ContentType, out var value)
+                    && string.Equals(value, "application/x-rtsp-tunnelled", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _logger.LogWarning("Invalid content-type header");
+                    client.Dispose();
+                    return;
+                }
+
+                var inError = session.UpdatePostChannel(client) switch
+                {
+                    RtspHttpServerTransport.UpdateState.Ok => false,
+                    RtspHttpServerTransport.UpdateState.NewSession => !_newConnections.TryAdd(session),
+                    _ => true,
+                };
+
+                if (inError)
+                {
+                    _logger.LogWarning("Removing session {sessionCookie} due to error", sessionCookie);
+                    session.Close();
+                    _activesSessions.TryRemove(sessionCookie, out session);
+                }
+            }
+            else
+            {
+                if (!headers.TryGetValue("Accept", out var value)
+                    && string.Equals(value, "application/x-rtsp-tunnelled", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _logger.LogWarning("Invalid accept header");
+                    client.Dispose();
+                    return;
+                }
+
+                var inError = session.UpdateGetChannel(client) switch
+                {
+                    RtspHttpServerTransport.UpdateState.Ok => false,
+                    RtspHttpServerTransport.UpdateState.NewSession => !_newConnections.TryAdd(session),
+                    _ => true,
+                };
+
+                await client.GetStream().WriteAsync(getResponse, cancellationToken).ConfigureAwait(false);
+
+                if (inError)
+                {
+                    _logger.LogWarning("Removing session {sessionCookie} due to error", sessionCookie);
+                    session.Close();
+                    _activesSessions.TryRemove(sessionCookie, out session);
+                }
+            }
+
+        }
+        catch (OperationCanceledException)
+        {
+            _logger.LogDebug("Operation canceled");
+        }
+        catch (IOException)
+        {
+            _logger.LogDebug("Error during read");
+        }
+    }
+
+    private static async Task<string> ReadOneLine(Stream stream, CancellationToken token)
+    {
+        // manual read stream, beause streamreader read too much data in the buffer
+        // So slowly read one by one
+        // 2048 is arbitrary, if a line of the http request is greater than 2048 
+        // the client is doing something stange.
+        byte[] buffer = new byte[2048];
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            int n = await stream.ReadAsync(buffer.AsMemory(i, 1), token).ConfigureAwait(false);
+            if (n != 1 || buffer[i] == '\n')
+            {
+                return Encoding.UTF8.GetString(buffer, 0, i);
+            }
+            if (buffer[i] == '\r')
+            {
+                // skip \r
+                i--;
+            }
+        }
+        throw new InvalidDataException("Line too long, invalid message");
+
+    }
+
+    private static async Task<Dictionary<string, string>> ReadHeaders(Stream stream, CancellationToken cancellationToken)
+    {
+        var headers = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        var headerLine = await ReadOneLine(stream, cancellationToken).ConfigureAwait(false);
+        while (!string.IsNullOrEmpty(headerLine))
+        {
+            var headerParts = headerLine.Split(':', 2);
+            if (headerParts.Length > 1) headers.Add(headerParts[0], headerParts[1]);
+            headerLine = await ReadOneLine(stream, cancellationToken).ConfigureAwait(false);
+        }
+
+        return headers;
+    }
+}
