f179ca12ee8021db2313545010f80546c7b7e9c5
Nicolas Graziano <nicolas.graziano@gmail.com>
2025-10-27 15:12:15 +0100

    Add a compatible argument list with previous version of CreateRtspTransportFromUrl

--- RTSP/RTSPUtils.cs
+++ RTSP/RTSPUtils.cs
@@ -25,7 +25,21 @@ namespace Rtsp
                 "rtsps" => new RtspTcpTlsTransport(uri, userCertificateSelectionCallback),
                 "http" => new RtspHttpTransport(uri, networkCredential),
                 "https" => new RTSPHttpsTransport(uri, networkCredential, userCertificateSelectionCallback),
-                _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri))
+                _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri)),
+            };
+        }
+
+        public static IRtspTransport CreateRtspTransportFromUrl(Uri uri, RemoteCertificateValidationCallback? userCertificateSelectionCallback = null)
+        {
+            // Keep compatible argument with previous version
+            // I need to check if any camera have authentification at http(s) level and not only at rtsp level
+            return uri.Scheme switch
+            {
+                "rtsp" => new RtspTcpTransport(uri),
+                "rtsps" => new RtspTcpTlsTransport(uri, userCertificateSelectionCallback),
+                "http" => new RtspHttpTransport(uri, new()),
+                "https" => new RTSPHttpsTransport(uri, new(), userCertificateSelectionCallback),
+                _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri)),
             };
         }
     }
