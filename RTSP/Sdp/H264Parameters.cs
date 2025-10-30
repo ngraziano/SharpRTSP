de0b2e959981041363139d976480db4f0dae719b
Nicolas Graziano <nicolas.graziano@gmail.com>
2025-10-30 12:35:05 +0100

    Reduce code duplication

--- RTSP/Sdp/H264Parameters.cs
+++ RTSP/Sdp/H264Parameters.cs
@@ -1,75 +1,18 @@
-﻿using System;
-using System.Collections;
+﻿namespace Rtsp.Sdp;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Rtsp.Sdp
+
+public class H264Parameters : ParametersBase, IDictionary<string, string>
 {
-    public class H264Parameters : IDictionary<string, string>
-    {
-        private const string HeaderName = "sprop-parameter-sets";
-        private readonly Dictionary<string, string> parameters = [];
+    private const string HeaderName = "sprop-parameter-sets";
 
-        public IList<byte[]> SpropParameterSets =>
-            TryGetValue(HeaderName, out var value)
-                ? value.Split(',').Select(Convert.FromBase64String).ToList()
-                : [];
+    public IList<byte[]> SpropParameterSets =>
+        TryGetValue(HeaderName, out var value)
+            ? value.Split(',').Select(Convert.FromBase64String).ToList()
+            : [];
 
-        public static H264Parameters Parse(string parameterString)
-        {
-            var result = new H264Parameters();
-            foreach (var pair in parameterString.Split(';').Select(x => x.Trim().Split('=', 2)))
-            {
-                if (!string.IsNullOrWhiteSpace(pair[0]))
-                    result[pair[0]] = pair.Length > 1 ? pair[1] : string.Empty;
-            }
-
-            return result;
-        }
-
-        public override string ToString()
-        {
-            return parameters.Select(p => p.Key + (p.Value != null ? "=" + p.Value : string.Empty))
-                .Aggregate((x, y) => x + ";" + y);
-        }
-
-        public string this[string index]
-        {
-            get => parameters[index];
-            set => parameters[index] = value;
-        }
-
-        public int Count => parameters.Count;
-
-        public bool IsReadOnly => ((IDictionary<string, string>)parameters).IsReadOnly;
-
-        public ICollection<string> Keys => ((IDictionary<string, string>)parameters).Keys;
-
-        public ICollection<string> Values => ((IDictionary<string, string>)parameters).Values;
-
-        public void Add(KeyValuePair<string, string> item) => ((IDictionary<string, string>)parameters).Add(item);
-
-        public void Add(string key, string value) => parameters.Add(key, value);
-
-        public void Clear() => parameters.Clear();
-
-        public bool Contains(KeyValuePair<string, string> item) =>
-            ((IDictionary<string, string>)parameters).Contains(item);
-
-        public bool ContainsKey(string key) => parameters.ContainsKey(key);
-
-        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex) =>
-            ((IDictionary<string, string>)parameters).CopyTo(array, arrayIndex);
-
-        public IEnumerator<KeyValuePair<string, string>> GetEnumerator() =>
-            ((IDictionary<string, string>)parameters).GetEnumerator();
-
-        public bool Remove(KeyValuePair<string, string> item) => ((IDictionary<string, string>)parameters).Remove(item);
-
-        public bool Remove(string key) => parameters.Remove(key);
-
-        public bool TryGetValue(string key, out string value) => parameters.TryGetValue(key, out value!);
-
-        IEnumerator IEnumerable.GetEnumerator() => ((IDictionary<string, string>)parameters).GetEnumerator();
-    }
+    public static H264Parameters Parse(string parameterString) => Parse<H264Parameters>(parameterString);
 }
--- RTSP/Sdp/H265Parameters.cs
+++ RTSP/Sdp/H265Parameters.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿namespace Rtsp.Sdp;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,90 +8,33 @@ using System.Linq;
 // Extract H265 fields
 // By Roger Hardiman, RJH Technical Consultancy Ltd
 
-namespace Rtsp.Sdp
+public class H265Parameters : ParametersBase, IDictionary<string, string>
 {
-    public class H265Parameters : IDictionary<string, string>
+    public IList<byte[]> SpropParameterSets
     {
-        private readonly Dictionary<string, string> parameters = [];
-
-        public IList<byte[]> SpropParameterSets
+        get
         {
-            get
+            List<byte[]> result = [];
+
+            if (ContainsKey("sprop-vps") && this["sprop-vps"] != null)
             {
-                List<byte[]> result = [];
-
-                if (ContainsKey("sprop-vps") && this["sprop-vps"] != null)
-                {
-                    result.AddRange(this["sprop-vps"].Split(',').Select(x => Convert.FromBase64String(x)));
-                }
-
-                if (ContainsKey("sprop-sps") && this["sprop-sps"] != null)
-                {
-                    result.AddRange(this["sprop-sps"].Split(',').Select(x => Convert.FromBase64String(x)));
-                }
-
-                if (ContainsKey("sprop-pps") && this["sprop-pps"] != null)
-                {
-                    result.AddRange(this["sprop-pps"].Split(',').Select(x => Convert.FromBase64String(x)));
-                }
-
-                return result;
+                result.AddRange(this["sprop-vps"].Split(',').Select(x => Convert.FromBase64String(x)));
             }
-        }
 
-        public static H265Parameters Parse(string parameterString)
-        {
-            var result = new H265Parameters();
-            foreach (var pair in parameterString.Split(';').Select(x => x.Trim().Split('=', 2)))
+            if (ContainsKey("sprop-sps") && this["sprop-sps"] != null)
             {
-                if (!string.IsNullOrWhiteSpace(pair[0]))
-                    result[pair[0]] = pair.Length > 1 ? pair[1] : string.Empty;
+                result.AddRange(this["sprop-sps"].Split(',').Select(x => Convert.FromBase64String(x)));
+            }
+
+            if (ContainsKey("sprop-pps") && this["sprop-pps"] != null)
+            {
+                result.AddRange(this["sprop-pps"].Split(',').Select(x => Convert.FromBase64String(x)));
             }
 
             return result;
         }
-
-        public override string ToString() =>
-            parameters.Select(p => p.Key + (p.Value != null ? "=" + p.Value : string.Empty))
-                .Aggregate((x, y) => x + ";" + y);
-
-        public string this[string index]
-        {
-            get => parameters[index];
-            set => parameters[index] = value;
-        }
-
-        public int Count => parameters.Count;
-
-        public bool IsReadOnly => ((IDictionary<string, string>)parameters).IsReadOnly;
-
-        public ICollection<string> Keys => ((IDictionary<string, string>)parameters).Keys;
-
-        public ICollection<string> Values => ((IDictionary<string, string>)parameters).Values;
-
-        public void Add(KeyValuePair<string, string> item) => ((IDictionary<string, string>)parameters).Add(item);
-
-        public void Add(string key, string value) => parameters.Add(key, value);
-
-        public void Clear() => parameters.Clear();
-
-        public bool Contains(KeyValuePair<string, string> item) =>
-            ((IDictionary<string, string>)parameters).Contains(item);
-
-        public bool ContainsKey(string key) => parameters.ContainsKey(key);
-
-        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex) =>
-            ((IDictionary<string, string>)parameters).CopyTo(array, arrayIndex);
-
-        public IEnumerator<KeyValuePair<string, string>> GetEnumerator() =>
-            ((IDictionary<string, string>)parameters).GetEnumerator();
-
-        public bool Remove(KeyValuePair<string, string> item) => ((IDictionary<string, string>)parameters).Remove(item);
-
-        public bool Remove(string key) => parameters.Remove(key);
-
-        public bool TryGetValue(string key, out string value) => parameters.TryGetValue(key, out value!);
-
-        IEnumerator IEnumerable.GetEnumerator() => ((IDictionary<string, string>)parameters).GetEnumerator();
     }
+
+    public static H265Parameters Parse(string parameterString) => Parse<H265Parameters>(parameterString);
+
 }
