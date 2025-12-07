22cf65228ed32a38e96b0884fe21099e2b9678a6
Nicolas Graziano <nicolas.graziano@gmail.com>
2025-12-07 19:14:06 +0100

    Register rtsps uri

--- RTSP/RTSPUtils.cs
+++ RTSP/RTSPUtils.cs
@@ -1,46 +1,51 @@
-﻿using System;
+﻿namespace Rtsp;
+
+using System;
 using System.Net;
 using System.Net.Security;
 
-namespace Rtsp
+public static class RtspUtils
 {
-    public static class RtspUtils
+    /// <summary>
+    /// Registers the rtsp scheùe for uri.
+    /// </summary>
+    public static void RegisterUri()
     {
-        /// <summary>
-        /// Registers the rtsp scheùe for uri.
-        /// </summary>
-        public static void RegisterUri()
+        if (!UriParser.IsKnownScheme("rtsp"))
         {
-            if (!UriParser.IsKnownScheme("rtsp"))
-            {
-                UriParser.Register(new HttpStyleUriParser(), "rtsp", 554);
-            }
+            UriParser.Register(new HttpStyleUriParser(), "rtsp", 554);
         }
 
-        public static IRtspTransport CreateRtspTransportFromUrl(Uri uri, NetworkCredential networkCredential, RemoteCertificateValidationCallback? userCertificateSelectionCallback = null)
+        if (!UriParser.IsKnownScheme("rtsps"))
         {
-            return uri.Scheme switch
-            {
-                "rtsp" => new RtspTcpTransport(uri),
-                "rtsps" => new RtspTcpTlsTransport(uri, userCertificateSelectionCallback),
-                "http" => new RtspHttpTransport(uri, networkCredential),
-                "https" => new RTSPHttpsTransport(uri, networkCredential, userCertificateSelectionCallback),
-                _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri)),
-            };
+            // Port 322 is indicated in RFC 7826 (we are RTSP 1.0 but we keep the same port)
+            UriParser.Register(new HttpStyleUriParser(), "rtsps", 322);
         }
+    }
 
-        public static IRtspTransport CreateRtspTransportFromUrl(Uri uri, RemoteCertificateValidationCallback? userCertificateSelectionCallback = null)
+    public static IRtspTransport CreateRtspTransportFromUrl(Uri uri, NetworkCredential networkCredential, RemoteCertificateValidationCallback? userCertificateSelectionCallback = null)
+    {
+        return uri.Scheme switch
         {
-            // Keep compatible argument with previous version
-            // I need to check if any camera have authentification at http(s) level and not only at rtsp level
-            return uri.Scheme switch
-            {
-                "rtsp" => new RtspTcpTransport(uri),
-                "rtsps" => new RtspTcpTlsTransport(uri, userCertificateSelectionCallback),
-                "http" => new RtspHttpTransport(uri, new()),
-                "https" => new RTSPHttpsTransport(uri, new(), userCertificateSelectionCallback),
-                _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri)),
-            };
-        }
+            "rtsp" => new RtspTcpTransport(uri),
+            "rtsps" => new RtspTcpTlsTransport(uri, userCertificateSelectionCallback),
+            "http" => new RtspHttpTransport(uri, networkCredential),
+            "https" => new RTSPHttpsTransport(uri, networkCredential, userCertificateSelectionCallback),
+            _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri)),
+        };
+    }
+
+    public static IRtspTransport CreateRtspTransportFromUrl(Uri uri, RemoteCertificateValidationCallback? userCertificateSelectionCallback = null)
+    {
+        // Keep compatible argument with previous version
+        // I need to check if any camera have authentification at http(s) level and not only at rtsp level
+        return uri.Scheme switch
+        {
+            "rtsp" => new RtspTcpTransport(uri),
+            "rtsps" => new RtspTcpTlsTransport(uri, userCertificateSelectionCallback),
+            "http" => new RtspHttpTransport(uri, new()),
+            "https" => new RTSPHttpsTransport(uri, new(), userCertificateSelectionCallback),
+            _ => throw new ArgumentException("The uri scheme is not supported", nameof(uri)),
+        };
     }
 }
