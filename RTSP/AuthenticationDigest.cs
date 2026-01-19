61b6e6f226aedf15ca50b095f6d83331620b2624
Roger Hardiman <software@rjh.org.uk>
2026-01-19 12:11:59 +0000

    Add ability to select SHA-256 Digest Authentication. Example in RtspCameraExample\RtspServer.cs
    Will send either
      WWW-Authenticate with an algorithm of MD5 (the default)
    or
      WWW-Authenticate with an alogirutm of SHA-256
    
    Tested with a patched FFmpeg
    
    TODO
    Some ONVIF cameras now support RTSP where they send _both_ WWW-Authenticate headers with MD5 first, and SHA-256 second. This is not yet supported.
    a) send a WWW-Authenticate with

--- RTSP/AuthenticationDigest.cs
+++ RTSP/AuthenticationDigest.cs
@@ -10,12 +10,15 @@ namespace Rtsp
     // WWW-Authentication and Authorization Headers
     public class AuthenticationDigest : Authentication
     {
+        public enum HASH_ALGORITHM { MD5, SHA256 };
+
         private readonly string _realm;
         private readonly string _nonce;
         private readonly string? _qop;
         private readonly string _cnonce;
+        private readonly HASH_ALGORITHM _algorithm;
 
-        public AuthenticationDigest(NetworkCredential credentials, string realm, string nonce, string? qop) : base(credentials)
+        public AuthenticationDigest(NetworkCredential credentials, string realm, string nonce, string? qop, HASH_ALGORITHM algorithm) : base(credentials)
         {
             _realm = realm ?? throw new ArgumentNullException(nameof(realm));
             _nonce = nonce ?? throw new ArgumentNullException(nameof(nonce));
@@ -27,40 +30,53 @@ namespace Rtsp
             }
             uint cnonce = (uint)Guid.NewGuid().GetHashCode();
             _cnonce = cnonce.ToString("X8");
+            _algorithm = algorithm;
         }
 
         public override string GetServerResponse()
         {
             //TODO implement correctly
-            return $"Digest realm=\"{_realm}\", nonce=\"{_nonce}\"";
+            string result = $"Digest realm=\"{_realm}\", nonce=\"{_nonce}\"";
+
+            // algorithm defaults to MD5 (some clients may not expect an algorithm=MD5 parameter
+            if (_algorithm == HASH_ALGORITHM.SHA256) result += $", algorithm=SHA-256";
+            return result;
         }
 
         public override string GetResponse(uint nonceCounter, string uri, string method,
             byte[] entityBodyBytes)
         {
-            MD5 md5 = MD5.Create();
-            string ha1 = CalculateMD5Hash(md5, $"{Credentials.UserName}:{_realm}:{Credentials.Password}");
+            HashAlgorithm hashAlgorithm;
+            if (_algorithm == HASH_ALGORITHM.SHA256)
+                hashAlgorithm = SHA256.Create();
+            else /* default is MD5 */
+                hashAlgorithm = MD5.Create();
+
+            string ha1 = CalculateHash(hashAlgorithm, $"{Credentials.UserName}:{_realm}:{Credentials.Password}");
             string ha2Argument = $"{method}:{uri}";
             bool hasQop = !string.IsNullOrEmpty(_qop);
 
             if (hasQop && _qop!.Equals("auth-int", StringComparison.InvariantCultureIgnoreCase))
             {
-                ha2Argument = $"{ha2Argument}:{CalculateMD5Hash(md5, entityBodyBytes)}";
+                ha2Argument = $"{ha2Argument}:{CalculateHash(hashAlgorithm, entityBodyBytes)}";
             }
-            string ha2 = CalculateMD5Hash(md5, ha2Argument);
+            string ha2 = CalculateHash(hashAlgorithm, ha2Argument);
 
             StringBuilder sb = new();
             sb.AppendFormat(CultureInfo.InvariantCulture, "Digest username=\"{0}\", realm=\"{1}\", nonce=\"{2}\", uri=\"{3}\"", Credentials.UserName, _realm, _nonce, uri);
             if (!hasQop)
             {
-                string response = CalculateMD5Hash(md5, $"{ha1}:{_nonce}:{ha2}");
+                string response = CalculateHash(hashAlgorithm, $"{ha1}:{_nonce}:{ha2}");
                 sb.AppendFormat(CultureInfo.InvariantCulture, ", response=\"{0}\"", response);
             }
             else
             {
-                string response = CalculateMD5Hash(md5, $"{ha1}:{_nonce}:{nonceCounter:X8}:{_cnonce}:{_qop}:{ha2}");
+                string response = CalculateHash(hashAlgorithm, $"{ha1}:{_nonce}:{nonceCounter:X8}:{_cnonce}:{_qop}:{ha2}");
                 sb.AppendFormat(CultureInfo.InvariantCulture, ", response=\"{0}\", cnonce=\"{1}\", nc=\"{2:X8}\", qop=\"{3}\"", response, _cnonce, nonceCounter, _qop);
             }
+
+            hashAlgorithm.Dispose();
+
             return sb.ToString();
         }
         public override bool IsValid(RtspRequest receivedMessage)
@@ -77,6 +93,7 @@ namespace Rtsp
                 string? nonce = null;
                 string? uri = null;
                 string? response = null;
+                HASH_ALGORITHM algorithm = HASH_ALGORITHM.MD5; // algorithm is optional, and defaults to MD5
 
                 foreach (string value in valueStr.Split(','))
                 {
@@ -106,14 +123,25 @@ namespace Rtsp
                     {
                         response = var;
                     }
+                    else if (tuple[0].Equals("algorithm", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (String.Equals(var, "MD5")) algorithm = HASH_ALGORITHM.MD5;
+                        else if (String.Equals(var,"SHA-256")) algorithm = HASH_ALGORITHM.SHA256;
+                    }
                 }
 
                 // Create the MD5 Hash using all parameters passed in the Auth Header with the 
                 // addition of the 'Password'
-                MD5 md5 = MD5.Create();
-                string hashA1 = CalculateMD5Hash(md5, username + ":" + realm + ":" + Credentials.Password);
-                string hashA2 = CalculateMD5Hash(md5, receivedMessage.RequestTyped + ":" + uri);
-                string expectedResponse = CalculateMD5Hash(md5, hashA1 + ":" + nonce + ":" + hashA2);
+                HashAlgorithm hashAlgorithm;
+                if (algorithm == HASH_ALGORITHM.SHA256)
+                    hashAlgorithm = SHA256.Create();
+                else /* Default to MD5 */
+                    hashAlgorithm = MD5.Create();
+
+                string hashA1 = CalculateHash(hashAlgorithm, username + ":" + realm + ":" + Credentials.Password);
+                string hashA2 = CalculateHash(hashAlgorithm, receivedMessage.RequestTyped + ":" + uri);
+                string expectedResponse = CalculateHash(hashAlgorithm, hashA1 + ":" + nonce + ":" + hashA2);
+                hashAlgorithm.Dispose();
 
                 // Check if everything matches
                 // ToDo - extract paths from the URIs (ignoring SETUP's trackID)
@@ -125,15 +153,14 @@ namespace Rtsp
             return false;
         }
 
-        // MD5 (lower case)
-        private static string CalculateMD5Hash(MD5 md5_session, string input)
+        private static string CalculateHash(HashAlgorithm hashAlgorithm, string input)
         {
             byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            return CalculateMD5Hash(md5_session, inputBytes);
+            return CalculateHash(hashAlgorithm, inputBytes);
         }
-        private static string CalculateMD5Hash(MD5 md5_session, byte[] input)
+        private static string CalculateHash(HashAlgorithm hashAlgorithm, byte[] input)
         {
-            byte[] hash = md5_session.ComputeHash(input);
+            byte[] hash = hashAlgorithm.ComputeHash(input);
 
             var output = new StringBuilder();
             foreach (var t in hash)
